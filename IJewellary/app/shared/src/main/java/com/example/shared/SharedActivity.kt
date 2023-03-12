4b84bc75e26e9b504f87fc5c55a8f515e59dd9c8
nahidhasan007 <teachpeach007@gmail.com>
2023-03-12 18:05:01 +0600

    gradle file updated

---
+++ IJewellary/app/shared/src/main/java/com/example/shared/SharedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shared
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared)
+    }
+}
